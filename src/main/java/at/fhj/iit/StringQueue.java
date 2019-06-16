5c6a03aa376f76de7286546bd4bc60f0f4b7a7e0
UllaO <48513570+UllaO@users.noreply.github.com>
2019-06-16 21:26:57 +0200

    StringQueue.java added
    Contains log4j commands.

--- src/main/java/at/fhj/iit/StringQueue.java
+++ src/main/java/at/fhj/iit/StringQueue.java
@@ -1,8 +1,21 @@
 package at.fhj.iit;
 
+
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Queue;
+
+import java.io.IOException;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
+
+
 
 // there's some Bugs included, try to debug the code and fix the Bugs
 // there are different Bugs, wrong implementation, typos, ...
@@ -12,7 +25,9 @@ import java.util.NoSuchElementException;
 /**
  * Class to implement a String queue
  */
-public class StringQueue implements Queue {
+public abstract class StringQueue implements Queue {
+	
+	private static final Logger logger = LogManager.getLogger(StringQueue.class);
 	
 	private List<String> elements = new ArrayList<String>();
 	private int maxSize;			//doesn't have to start with a value if the constructor method demands said value
@@ -22,6 +37,8 @@ public class StringQueue implements Queue {
 	 * @param maxSize defines the maximum size the queue can have
 	 */
 	public StringQueue(int maxSize){	//variable maxsize was not used because it was written with s instead of S
+		
+		logger.info("constructor with maxSize" + maxSize);
 		this.maxSize = maxSize;
 	}
 
@@ -35,8 +52,10 @@ public class StringQueue implements Queue {
 		if(elements.size()!= maxSize)
 			elements.add(obj);
 		else
+			
+			logger.error("element not added");
 			return false;
-		
+		logger.info("element added");
 		return true;
 	}
 
@@ -49,6 +68,8 @@ public class StringQueue implements Queue {
 		String element = peek();
 		
 		if(elements.size() != 0){			//size() == 0 is wrong needs to be !=
+			logger.error("wrong size");
+			
 			elements.remove(0);
 		}
 		
@@ -62,6 +83,8 @@ public class StringQueue implements Queue {
 	@Override
 	public String remove() {
 		String element = poll();		//element should not be overwritten so removed 'element = "";'
+		logger.error("removed 'element = ''");
+		
 		if(element == null)
 			throw new NoSuchElementException("there's no element any more");
 		
@@ -79,7 +102,7 @@ public class StringQueue implements Queue {
 			element = elements.get(0);
 		else
 			element = null;
-		
+		logger.info("returns top element");
 		return element;
 	}
 
@@ -92,7 +115,7 @@ public class StringQueue implements Queue {
 		String element = peek();
 		if(element == null)
 			throw new NoSuchElementException("there's no element any more");
-		
+		logger.info("returns top element");
 		return element;
 	}
 
